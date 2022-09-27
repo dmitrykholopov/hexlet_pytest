b7f3417c56b67274a13f3c4ab7a5c7d4a6b0b109
Dmitry Kholopov <dkholopov@yandex.ru>
2022-09-27 23:35:03 +0300

    add test_stack

--- example.py
+++ example.py
@@ -1,8 +1,2 @@
-# import sys
-
-
 def reverse(string):
     return string[::-1]
-
-# print(reverse('Hexlet'))
-# print(sys.path)
--- tests/test_example.py
+++ tests/test_example.py
@@ -6,4 +6,3 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
-
